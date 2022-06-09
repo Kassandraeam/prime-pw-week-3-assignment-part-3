24080158cc7fca8ae157cafac89fd37ec2ab8810
Kas Eam <KassandraEam@gmail.com>
2022-06-09 10:48:57 -0500

    Completed problems 1-8.

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -42,10 +42,29 @@ for (let x=0; x<supplyChanges.length; x++){
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
+for (i of supplyChanges){
+    if (i>0){
+        console.log(`Added ${i} parts.`);
+    } else if (i === 0){
+        console.log('No change.');
+    } else if (i < 0){
+        console.log(`Removed ${i} parts.`)
+    }
+};
+/*
+for (variable of iterable) {
+  statement
+}
+*/
 
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
+let totalParts = 0;
+for (let i=0; i<supplyChanges.length; i++){
+    totalParts += supplyChanges[i];
+}
+console.log(`The total number of supplies is: ${totalParts}`);
 
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
@@ -54,3 +73,4 @@ console.log('8. Total supplies available is:');
 //    no more boxes can be filled.
 //    Then log how many boxes were filled, and how many parts are left over.
 console.log('9. Filling boxes with a "while" loop');
+
